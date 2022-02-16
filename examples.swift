6b970c1212c763ec88ec097b60cb0ab86d4a4ab9
bastianh <bastianhoejbjerre@gmail.com>
2022-02-16 14:31:09 +0800

    First examples

---
+++ examples.swift
@@ -0,0 +1,11 @@
+func addTwoNumbers(number1: Int, number2: Int) -> Int {
+    return number1 + number2
+}
+
+func greeting(name: String) -> String {
+    return "Hi " + name
+}
+
+func isDivisableByTwo(number: Int) -> Bool {
+    return number % 2 == 0
+}
